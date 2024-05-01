e8083f03ecae01d85cf57d0ac3f457a2705a1595
Hetomy125 <aswet125@gmail.com>
2024-05-01 13:09:27 +0400

    commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function setNumber(number) {
+    let resultString = documet.getElementById('result');
+    let currentText = resultString.tetxContent;
+    resultString.innerText = currentText = number
+}
